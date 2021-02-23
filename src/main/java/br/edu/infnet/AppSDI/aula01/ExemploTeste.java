178ebb84902d3e2651012225b2ffac4dcffbed20
Marcelo-Carlos <Marcelo Carlos>
2021-02-23 09:11:46 -0300

    Alteracao class test

--- src/main/java/br/edu/infnet/AppSDI/aula01/ExemploTeste.java
+++ src/main/java/br/edu/infnet/AppSDI/aula01/ExemploTeste.java
@@ -1,14 +1,30 @@
 package br.edu.infnet.AppSDI.aula01;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExemploTeste {
 	
 	public static void main(String[] args) {
 		
-		Aluno aluno = new Aluno();
-		aluno.setId(1);
-		aluno.setNome("Marcelo");
-		aluno.setEmail("marcelo@gmail.com");
-		System.out.println(aluno);
+		String[] nomes = {"Marcelo", "Maria", "Carlos"};
+		String[] emails = {"msac@gmail.com", "aaa@gmail.com", "bbb@gmail.com"};
+		
+		List<Aluno> alunos = new ArrayList<Aluno>();
+		
+		for (int i = 0; i < nomes.length; i++) {
+			
+			Aluno aluno = new Aluno();
+			aluno.setId(i+1);
+			aluno.setNome(nomes[i]);
+			aluno.setEmail(emails[i]);
+			alunos.add(aluno);
+			
+		}
+		
+		for (Aluno item : alunos) {
+			System.out.println(item);
+		}
 		
 	}
 }
