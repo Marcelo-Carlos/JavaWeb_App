12c0ea3f92a5dbf942851c7c992f080bdfd43e17
Marcelo-Carlos <Marcelo Carlos>
2021-02-23 08:42:30 -0300

    Criacao do projeto Java

---
+++ src/main/java/br/edu/infnet/AppSDI/aula01/ExemploTeste.java
@@ -0,0 +1,14 @@
+package br.edu.infnet.AppSDI.aula01;
+
+public class ExemploTeste {
+	
+	public static void main(String[] args) {
+		
+		Aluno aluno = new Aluno();
+		aluno.setId(1);
+		aluno.setNome("Marcelo");
+		aluno.setEmail("marcelo@gmail.com");
+		System.out.println(aluno);
+		
+	}
+}
---
+++ src/test/java/br/edu/infnet/AppSDI/AppSdiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.AppSDI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSdiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
